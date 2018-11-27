efdd29672f04d6c5bbbb6b8c042abce9e173204e
Munchery-OliverJesusDeQuintos <oliver@munchery.com>
2018-11-27 23:11:35 +0800

    Add config and bot class

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Talk2meChatbot
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.paths.add File.join('app', 'bot'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'bot', '*')]
   end
 end
