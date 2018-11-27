bc2b56a8b39ff0ce0c4d002fb5474f5a29f9b32e
Munchery-OliverJesusDeQuintos <oliver@munchery.com>
2018-11-28 00:30:29 +0800

    Add changes for testing

--- app/bot/example.rb
+++ app/bot/example.rb
@@ -1,5 +1,11 @@
 include Facebook::Messenger
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
+  # message.reply(text: 'Hello, human!')
+  Bot.deliver({
+    recipient: message.sender,
+    message: {
+      text: message.text
+    }
+  }, access_token: ENV["ACCESS_TOKEN"])
 end
