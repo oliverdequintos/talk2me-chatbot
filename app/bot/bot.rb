f468e5e9aba21508eeb552cb74467773997a26cb
Munchery-OliverJesusDeQuintos <oliver@munchery.com>
2018-11-27 23:31:36 +0800

    change

--- app/bot/bot.rb
+++
@@ -1,9 +0,0 @@
-require 'facebook/messenger'
-
-include Facebook::Messenger
-
-Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
-
-Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
-end
